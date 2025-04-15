43587a586b0b44199a3e0a98556a10fe2bd8acc1
Akshat-Aggarwal-2003 <83298479+Akshat-Aggarwal-2003@users.noreply.github.com>
2025-04-16 00:23:59 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["www.shadcnblocks.com", "flowbite.s3.amazonaws.com", "unsplash.com", "cdn.sanity.io"],
+  },
+};
+
+export default nextConfig;
